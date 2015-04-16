988fe994c8e5fff34836b2fd8da6f04bcc0a5880
Jan Paul <jan.paul1@gmx.net>
2015-04-16 10:47:27 +0200

    TestData reader

--- Buchempfehlung/src/com/dhbw/wbs/Main.java
+++ Buchempfehlung/src/com/dhbw/wbs/Main.java
@@ -1,8 +1,19 @@
 package com.dhbw.wbs;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Must enter path to testifle");
+            System.exit(0);
+        }
+        else
+        {
+            TestData testData = new TestData(args[0]);
+        }
+        
     }
 }
---
+++ Buchempfehlung/src/com/dhbw/wbs/TestData.java
@@ -0,0 +1,45 @@
+package com.dhbw.wbs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestData
+{
+    BufferedReader reader;
+    String line;
+
+    public TestData(String file)
+    {
+        try
+        {
+            reader = new BufferedReader(new FileReader(file));
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public String[] getNextTestDataEntry()
+    {
+        String[] data = new String[0];
+
+        try
+        {
+            if ((line = reader.readLine()) != null)
+            {
+                //Alter, Geschlecht, Verheiratet, Kinderzahl, Abschluss, Beruf, Einkommen, Buch
+                data = line.split(",");
+            }
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+}
+
+
